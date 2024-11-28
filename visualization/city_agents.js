486afdb6ede457f113eb9657abe491d29845a6e5
Miguel Soria <a01028033@tec.mx>
2024-11-28 10:24:50 -0600

    fix: Fixed spawn rotations (again), added pastel random colors to buildings)

--- visualization/city_agents.js
+++ visualization/city_agents.js
@@ -322,13 +322,20 @@ async function getObstacles() {
       if (obstacles.length == 0) {
         // Create new obstacles and add them to the obstacles array
         for (const obstacle of result.positions) {
+          const randomColor2 = [
+            Math.random() * 0.5 + 0.5,
+            Math.random() * 0.5 + 0.5,
+            Math.random() * 0.5 + 0.5,
+            0.5, // Valor alfa
+          ];
+
           const newObstacle = new Object3D(
             obstacle.id,
             [obstacle.x, obstacle.y, obstacle.z],
             [0, 0, 0],
             [0.6, 0.8, 0.6],
             [0.2, 0.2, 0.2, 1], // ambientColor
-            [0.5, 0.5, 0.8, 1], // diffuseColor
+            randomColor2, // diffuseColor
             [0.3, 0.3, 0.3, 1], // specularColor
             160, // shininess
           ); // Gray color
@@ -400,7 +407,7 @@ async function getLights() {
             [0, 0, 0],
             [1, 0.2, 1],
             [0.1, 0.1, 0.1, 1], // ambientColor
-            [0.3, 0.3, 0.3, 1], // diffuseColor
+            [0.1, 0.1, 0.1, 1], // diffuseColor
             [0.3, 0.3, 0.3, 1], // specularColor
             400, // shininess
           ); // Dark gray color
@@ -489,7 +496,7 @@ async function getRoads() {
           [0, 0, 0],
           [1, 0.2, 1],
           [0.1, 0.1, 0.1, 1], // ambientColor
-          [0.3, 0.3, 0.3, 1], // diffuseColor
+          [0.1, 0.1, 0.1, 1], // diffuseColor
           [0.3, 0.3, 0.3, 1], // specularColor
           400, // shininess
         ); // Dark gray color
